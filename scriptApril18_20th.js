b6a7b5b7bd24ab3919d82119289cd8cee962cf31
Ruth <awalkwithwellbeings@yahoo.com>
2023-04-24 20:22:46 +0100

    Update scriptApril18_20th.js

--- scriptApril18_20th.js
+++ scriptApril18_20th.js
@@ -6,25 +6,23 @@ function myFunction() {
 }
 */
 function checkAnswers() {
-   
     //assign "0" to the "score" variable
     var score = 0;
     //assign the quiz elements to the quiz variable
     quiz = document.forms.index.elements;
     //assign the answer to the quation to the answer1 variable
     answer1 = quiz.growth.value;
-    //check the answer to the first question       
-
+    //check the answer to the first question 
        if(answer1 == "100%") {
             score = score +1;
-} 
+       }
     answer2 = quiz.talent.value;
-    
-         if (answer2 =="100") {
+        if (answer2 =="100") {
             score = score + 1;
         }
-     alert("Well done, your score ..." + score);
-}
+        alert("Well done, your score ..." + score);
+
+    }
 
 
 
