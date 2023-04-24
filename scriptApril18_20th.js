905881e2daddc060baed7dbb46f679786c80ce81
Ruth <awalkwithwellbeings@yahoo.com>
2023-04-24 19:50:14 +0100

    Update scriptApril18_20th.js

--- scriptApril18_20th.js
+++ scriptApril18_20th.js
@@ -6,71 +6,25 @@ function myFunction() {
 }
 */
 function checkAnswers() {
-    alert("Well done, your score was 100% + score");
-}
+   
     //assign "0" to the "score" variable
     var score = 0;
     //assign the quiz elements to the quiz variable
     quiz = document.form.QuizDraft2.elements;
     //assign the answer to the quation to the answer1 variable
-    answer1 = QuizDraft2.growth.value;
-    //check the answer to the first question
-        if(answer1 == "20%") {
-            score = score ;
-        }
-        if(answer1 == "40%") {
-            //add 1 to the score if the user score  was correct
-            score = score ;
-       }
-       if(answer1 == "60%") {
-            score = score ;
-       }
-       if(answer1 == "80%"){
-            score = score ;
-       }
-       if(answer1 == "100%")
-            score = score +1
-    
+    answer1 = quiz.growth.value;
+    //check the answer to the first question       
+
+       if(answer1 == "100%") {
+            score = score +1;
+} 
     answer2 = quiz.talent.value;
     
-        if (answer2 =="10") {
-            score = score ;
-        }
-        answer2 = quiz.talent.value;
-        if (answer2 =="20") {
-            score = score ;
-        }
-        answer2 = quiz.talent.value;
-        if (answer2 =="30") {
-            score = score ;
-        }answer2 = quiz.talent.value;
-        if (answer2 =="40") {
-            score = score ;
-        }
-        answer2 = quiz.talent.value;
-        if (answer2 =="50") {
-            score = score ;
-        }
-        answer2 = quiz.talent.value;
-        if (answer2 =="60") {
-            score = score ;
-        }
-        answer2 = quiz.talent.value;
-        if (answer2 =="70") {
-            score = score ;
-        }
-        answer2 = quiz.talent.value;
-        if (answer2 =="80") {
-            score = score ;
-        }
-        answer2 = quiz.talent.value;
-        if (answer2 =="90") {
-            score = score ;
-        }
-        answer2 = quiz.talent.value;
-        if (answer2 =="100") {
+         if (answer2 =="100") {
             score = score + 1;
         }
+     alert("Well done, your score ..." + score);
+}
 
 
 
