4166465697bddf03856b7fd5aa9594e65b667bfe
Ruth <awalkwithwellbeings@yahoo.com>
2023-04-26 20:07:00 +0100

    Update and rename scriptT to scriptT.js

--- scriptT.js
+++ scriptT.js
@@ -1,16 +1,14 @@
 function myFunction() {
     alert("Thank you for completing the quiz");
 }
-/*function finishFunction(); {
-    alert("Thank you for completing the quiz.");
-}
 
 function checkAnswers() {
   var score = 0;
   quiz = document.forms.index.elements;
-  answer1 = quiz.growth.value;
-    if(answer1 == "100%") {
-    score = score +1;
+  
+    answer1 = quiz.growth.value;
+      if(answer1 == "100%") {
+      score = score +1;
  }
     answer2 = quiz.talent.value;
       if (answer2 =="100") {
