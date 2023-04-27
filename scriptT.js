b5d62167e0ec2d5cebbb142e107499d1c887c332
Ruth <awalkwithwellbeings@yahoo.com>
2023-04-27 23:03:42 +0100

    Update scriptT.js

--- scriptT.js
+++ scriptT.js
@@ -6,13 +6,21 @@ function checkAnswers() {
   var score = 0;
   quiz = document.forms.index.elements;
   
-    answer1 = quiz.growth.value;
-      if(answer1 == "100%") {
+    answer1 = quiz.people.value;
+      if (answer1 == "100%") {
       score = score +1;
- }
-    answer2 = quiz.talent.value;
-      if (answer2 =="100") {
+}
+    answer2 = quiz.clearMind.value;
+      if (answer2 == "75") {
       score = score + 1;
+}
+    answer3 = quiz.kindness.value;
+    if (answer3 =="90%") {
+    score = score + 1;
+}
+    answer4 = quiz.goodhearts.value;
+        if (answer4 =="100") {
+        score = score + 1;
 }
    alert("Well done, your score is..." + score);
 }
